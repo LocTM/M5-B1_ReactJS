2f09e630b471ea80d94747f6e59cdfa79bad3a85
LocTM <tranminhloc.anhk@gmail.com>
2024-03-31 20:37:08 +0700

    test 2

--- js.js
+++ js.js
@@ -1,4 +1,11 @@
 function test(x = 'hello'){
     console.log(x);
 }
-test();
+test('abc');
+test('LocTranMinh');
+test('Tran Minh Loc');
+
+function test2(x = "default"){
+    console.log(x);
+}
+test2("Test lan 2")
